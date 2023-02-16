fd95b08f0e38d76fa581cc8c535db10bc53ea262
Youssef El Toukhi <youssefeltoukhi@hotmail.com>
2023-02-16 18:51:15 +0400

    Implemented combine_strs. There is still a bug regarding quotes

--- tokenize.c
+++ tokenize.c
@@ -6,7 +6,7 @@
 /*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 14:31:18 by yel-touk          #+#    #+#             */
-/*   Updated: 2023/02/16 15:05:45 by youssef          ###   ########.fr       */
+/*   Updated: 2023/02/16 17:07:05 by youssef          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,16 @@ int	is_redir(char c)
 	return (0);
 }
 
-void	check_dquotes(char c, int s_quote, int *d_quote)
+void	check_quotes(char c, int *s_quote, int *d_quote)
 {
-	if (c == '\"' && !s_quote && !*d_quote)
-		*d_quote = 1; //return(1);
-	else if (c == '\"' && !s_quote)
-		*d_quote = 0; //return (0);
-	// return (d_quote);
-}
-
-void	check_squotes(char c, int *s_quote, int d_quote)
-{
-	if (c == '\'' && !d_quote && !*s_quote)
-		*s_quote = 1;//return(1);
-	else if (c == '\'' && !d_quote)
-		*s_quote = 0; //return(0);
-	//return (s_quote);
+	if (c == '\"' && !*s_quote && !*d_quote)
+		*d_quote = 1;
+	else if (c == '\"' && !*s_quote)
+		*d_quote = 0;
+	if (c == '\'' && !*d_quote && !*s_quote)
+		*s_quote = 1;
+	else if (c == '\'' && !*d_quote)
+		*s_quote = 0;
 }
 
 static int	get_num_tokens(char const *s, char c)
@@ -56,8 +50,7 @@ static int	get_num_tokens(char const *s, char c)
 		return (0);
 	while (s[i] != 0)
 	{
-		check_squotes(s[i], &s_quote, d_quote);
-		check_dquotes(s[i], s_quote, &d_quote);
+		check_quotes(s[i], &s_quote, &d_quote);
 		if (((s[i] == c || s[i] == '|' || (is_redir(s[i]) && !is_redir(s[i + 1])))
 			&& s[i + 1] != 0 && s[i + 1] != c && !d_quote && !s_quote)
 			|| (((is_redir(s[i + 1]) && !is_redir(s[i])) || s[i + 1] == '|') && !d_quote && !s_quote))
@@ -95,31 +88,41 @@ char	**malloc_fail(char **res, int i)
 // 	return (j);
 // }
 
-char	*combine_strs(int j, int num, const char *s, int opened_q)
+char	*combine_strs(int j, int num, const char *s)
 {
-	printf("combine_str called\n");
-	(void) j;
-	(void) num;
-	(void) s;
-	(void) opened_q;
-	// char	*read;
-	// int	d_quote;
-	// int	s_quote;
+	char	*read;
+	char	*res;
+	int		d_quote;
+	int		s_quote;
 
-	// read = ft_substr(s, j - num, num);
-	// while (s[j] && s[j] != ' ')
-	// {
-	// 	while (s[j] && (opened_q || d_quote || s_quote))
-	// 	{
-	// 		//read until quote closed
-	// 		j++;
-	// 	}
-	// 	opened_q = 0;
-	// 	while (s[j] && s[j] != ' ' && s[j] != '\'' && s[j] != '\"')
-	// 	{
-	// 		// "keine ahnung";
-	// 	}
-	// }
+	printf("\n");
+	s_quote = 0;
+	d_quote = 0;
+	// check_quotes(s[j], &s_quote, &d_quote);
+	read = ft_substr(s, j - num, num);
+	res = NULL;
+	num = 0;
+	if (!read)
+		return (NULL);
+	// j++;
+	while (s[j] && s[j] != ' ' && s[j] != '|' && !is_redir(s[j]))
+	{
+		printf("read: %s\n", read);
+		while (s[j] && !s_quote && !d_quote && s[j] != ' ' && s[j] != '|' && !is_redir(s[j]))
+			check_quotes(s[j++], &s_quote, &d_quote);
+		while (s[j] && ((s_quote && s[j] != '\'') || (d_quote && s[j] != '\"')))
+		{
+			check_quotes(s[j++], &s_quote, &d_quote);
+			num++;
+		}
+		check_quotes(s[j], &s_quote, &d_quote);
+		res = ft_strjoin(read, ft_substr(s, j - num, num));
+		free(read);
+		read = res;
+		j++;
+		printf("res: %s\n", res);
+	}
+	printf("\n");
 	return NULL;
 }
 
@@ -145,8 +148,7 @@ char	**split_tokens(char const *s, char c, char ***res)
 			|| (s[j] == '\'' && (s[j + 1] == '\'') || s_quote)))
 		{
 			printf("letter: %c, sq: %d, dq: %d\n", s[j], s_quote, d_quote);
-			check_squotes(s[j], &s_quote, d_quote);
-			check_dquotes(s[j], s_quote, &d_quote);
+			check_quotes(s[j], &s_quote, &d_quote);
 			j++;
 		}
 		printf("entering while-loop 2\n");
@@ -162,15 +164,10 @@ char	**split_tokens(char const *s, char c, char ***res)
 		// 	(*res)[i] = combine_strs(j, num, s, s_quote);
 		// else if ((s[j] == '\"' && !s_quote) && s[j + 1] != c)
 		// 	(*res)[i] = combine_strs(j, num, s, d_quote);
-		if (s[j] == '\'')
+		if (s[j] == '\'' || s[j] == '\"')
 		{
-			printf("combine called\n"); //(*res)[i] = combine_strs(j, num, s, s_quote);
-			i++;
-			continue;
-		}
-		if (s[j] == '\"')
-		{
-			printf("combine called\n"); // (*res)[i] = combine_strs(j, num, s, d_quote);
+			printf("combine called\n");
+			(*res)[i] = combine_strs(j, num, s);
 			i++;
 			continue;
 		}
@@ -188,11 +185,11 @@ char	**split_tokens(char const *s, char c, char ***res)
 			}
 		}
 		(*res)[i] = ft_substr(s, j - num, num);
-		printf("word: %s\n", (*res)[i]);
 		if ((*res)[i] == NULL)
 			return (malloc_fail(*res, i));
-		if (s[j] == c || s[j] == '\'' || s[j] == '\"')
-			j++;
+		printf("word: %s\n", (*res)[i]);
+		// if (s[j] == c)
+		// 	j++;
 		i++;
 	}
 	(*res)[i] = 0;
@@ -218,7 +215,7 @@ int main()
 	// char *s = "echo \'hi\'f\'\" \"\"\'\"\'\"\'there\'  ";
 	// char *s = "\" \"\'\'\"gr\"ep||echo>h>>< | \'| hi>|\'there  ";
 	char *s = "\'\"\"\'e\' cho hi\'|hi >>>there";//"\"\"echo>\"\" \"hi\"";
-	// char *s = "echo hi \"\" there\"     s\"\"\'$x\"";
+	// char *s = " echo hi \"\" there\"     s\"\"\'$x\"";
 	printf("%s\n", s);
 	printf("%d\n\n", get_num_tokens(s, ' '));
 	char **r = tokenize(s);
