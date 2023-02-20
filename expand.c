74c9d7af0083358d1ad232c7b765077f9f7cbd14
Youssef <youssefeltoukhi@hotmail.com>
2023-02-20 14:58:34 +0400

    Expansion is now implemented. 0 However is not handeled correctly.

--- expand.c
+++ expand.c
@@ -91,11 +91,43 @@ int	get_expanded_len(char *line, t_list *lst)
 	return (len);
 }
 
-char	*expand_line(char *line, int len)
+char	*expand_line(char *line, int len, t_list *lst)
 {
 	char *new_line;
+	int		s_quote;
+	int		d_quote;
+	int		i;
+	int		j;
+	char	*var_name;
 
-	// new_line = malloc(sizeof(char) * (len + 1));
+	new_line = malloc(sizeof(char) * (len + 1));
+	if (!new_line)
+		return (0);
+	s_quote = 0;
+	d_quote = 0;
+	i = -1;
+	j = 0;
+	while(line[++i])
+	{
+		check_quotes(line[i], &s_quote, &d_quote);
+		if (line[i] == '$' && !s_quote
+			&& ((line[i + 1] >= 'A' && line[i + 1] <= 'Z')
+			|| (line[i + 1] >= 'a' && line[i + 1] <= 'z')
+			|| (line[i + 1] == '_' && line[i + 2] != '\0' && line[i + 2] != ' '
+			&& line[i + 2] != '\'' &&line[i + 2] != '\"')))
+		{
+			var_name = get_var_name(&line[i]);
+			if (!var_name)
+				return (0);
+			ft_memcpy(&new_line[j], get_value(var_name, lst), ft_strlen(get_value(var_name, lst)));
+			i += ft_strlen(var_name);
+			j += ft_strlen(get_value(var_name, lst));
+			free(var_name);
+			continue;
+		}
+		new_line[j++] = line[i];
+	}
+	new_line[j] = '\0';
 	return (new_line);
 }
 
@@ -104,25 +136,26 @@ char	*expand(char *line, t_list *lst)
 	char	*new_line;
 	int		len;
 
-	// if (!should_expand(line))
-	// 	return (line);
+	if (!should_expand(line))
+		return (0);
 	len = get_expanded_len(line, lst);
 	if (len == -1)
 		return (0);
-	new_line = expand_line(line, len);
-	free(line);
-	return (0);
+	new_line = expand_line(line, len, lst);
+	if (!new_line)
+		return (0);
+	return (new_line);
 }
 
-int main()
-{
-	// char *s = "echo \'hi\'f\'\" \"\"\'\"\'\"\'there\'  ";
-	char *s = "echo e$ffs";
-	// char *s = "\"\"echo - | > \"a\" \"hi\"";//"he\'\"\"\'e\' cho hi\'|hi >>there";
-	// char *s = " echo hi \"\" there\"     s\"\"\'$x\"";
-	// char *s = "ec\"\"\'\'ho";
-	printf("%s\n", s);
-	// printf("should_expand: %d\n", should_expand(s));
-	// get_expanded_len(s, -1);
-	// printf("name: %s\n\n", get_var_name(s));
-}
+// int main()
+// {
+// 	// char *s = "echo \'hi\'f\'\" \"\"\'\"\'\"\'there\'  ";
+// 	char *s = "echo e$ffs";
+// 	// char *s = "\"\"echo - | > \"a\" \"hi\"";//"he\'\"\"\'e\' cho hi\'|hi >>there";
+// 	// char *s = " echo hi \"\" there\"     s\"\"\'$x\"";
+// 	// char *s = "ec\"\"\'\'ho";
+// 	printf("%s\n", s);
+// 	// printf("should_expand: %d\n", should_expand(s));
+// 	// get_expanded_len(s, -1);
+// 	// printf("name: %s\n\n", get_var_name(s));
+// }
