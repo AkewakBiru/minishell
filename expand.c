e29d63b6a633f555b465a2b93746a306953df09b
Youssef <youssefeltoukhi@hotmail.com>
2023-02-20 13:47:22 +0400

    Implemented get_var_name()

--- expand.c
+++ expand.c
@@ -11,25 +11,24 @@
 char	*get_var_name(char *line)
 {
 	int		i;
-	// int		len;
 	char	*var_name;
 
 	i = 1;
-	// len = 0;
 	while(line[i] && line[i] != ' ' && line[i] != '$' && line[i] != '<'
-		&& line[i] != '>' && line[i] != '|' && line[i] != '\'' && line[i] != '\"')
-	{
-		// len++;
+			&& line[i] != '>' && line[i] != '|' && line[i] != '\'' && line[i] != '\"')
 		i++;
-	}
-	var_name = malloc(sizeof(char) * (i));//(len + 1));
-
-	
+	var_name = malloc(sizeof(char) * (i));
+	if (!var_name)
+		return (0);
+	var_name[--i] = '\0';
+	while (--i >= 0)
+		var_name[i] = line[i + 1];
+	return(var_name);
 }
 
 char	*get_value(char *key, t_list *lst)
 {
-
+	return (0);
 }
 
 int	should_expand(char *line)
@@ -54,12 +53,12 @@ int	should_expand(char *line)
 	return (0);
 }
 
-int	get_expanded_len(char *line, t_list *lst)
+int	get_expanded_len(char *line)//, t_list *lst)
 {
-	int	i;
-	int	s_quote;
-	int	d_quote;
-	int	len;
+	int		i;
+	int		s_quote;
+	int		d_quote;
+	int		len;
 	char	*var_name;
 	// int	expanded;
 
@@ -77,7 +76,8 @@ int	get_expanded_len(char *line, t_list *lst)
 			|| (line[i + 1] == '_' && line[i + 2] != '\0' && line[i + 2] != ' '
 			&& line[i + 2] != '\'' &&line[i + 2] != '\"')))
 		{
-			var_name = get_var_name(line[i]);
+			var_name = get_var_name(&line[i]);
+			printf("name: %s\n", var_name);
 		}
 
 	}
@@ -99,7 +99,7 @@ char	*expand(char *line, t_list *lst)
 
 	// if (!should_expand(line))
 	// 	return (line);
-	len = get_expanded_len(line, lst);
+	len = get_expanded_len(line);//, lst);
 	if (!len)
 		return (0);
 	new_line = expand_line(line, len);
@@ -110,10 +110,12 @@ char	*expand(char *line, t_list *lst)
 int main()
 {
 	// char *s = "echo \'hi\'f\'\" \"\"\'\"\'\"\'there\'  ";
-	char *s = "echo e\'$s\'";
+	char *s = "echo e$ffs";
 	// char *s = "\"\"echo - | > \"a\" \"hi\"";//"he\'\"\"\'e\' cho hi\'|hi >>there";
 	// char *s = " echo hi \"\" there\"     s\"\"\'$x\"";
 	// char *s = "ec\"\"\'\'ho";
 	printf("%s\n", s);
-	printf("should_expand: %d\n", should_expand(s));
+	// printf("should_expand: %d\n", should_expand(s));
+	get_expanded_len(s);
+	// printf("name: %s\n\n", get_var_name(s));
 }
