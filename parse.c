877a2645005ad33c8f2466dbfeb1b1534b6e464c
Youssef <youssefeltoukhi@hotmail.com>
2023-03-01 16:24:58 +0400

    Parse fixed for norminette

--- parse.c
+++ parse.c
@@ -6,22 +6,11 @@
 /*   By: yel-touk <yel-touk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:04:45 by yel-touk          #+#    #+#             */
-/*   Updated: 2023/03/01 13:25:50 by yel-touk         ###   ########.fr       */
+/*   Updated: 2023/03/01 16:24:28 by yel-touk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-// t_token	**empty_token()
-// {
-// 	t_token	**res;
-
-// 	res = malloc(sizeof(t_token *));
-// 	if (!res)
-// 		return (NULL);
-// 	res[0] = NULL;
-// 	return (res);
-// }
 
 t_token	**parse(char *line, t_list *lst, t_ints *t_int)
 {
@@ -39,40 +28,18 @@ t_token	**parse(char *line, t_list *lst, t_ints *t_int)
 	if (!tokens)
 		return (NULL);
 	if (!tokens[0])
-		return tokens;
+		return (tokens);
 	if (label_tokens(&tokens))
 	{
 		free_tokens(&tokens);
 		return (NULL);
 	}
+	return (tokens);
+}
+
 	// int i = 0;
 	// while (tokens && tokens[i])
 	// {
 	// 	printf("token: %s, type: %u\n", tokens[i]->token, tokens[i]->type);
 	// 	i++;
 	// }
-	return (tokens);
-}
-
-// int main()
-// {
-// 	char *s = "echo \'hi\'f\'\" \"\"\'\"\'\"\'there\'  ";
-// 	// char *s = "\" \"\'\'\"gr\"ep||echo>h>>< | \'| hi>|\'there  ";
-// 	// char *s = "\"\"echo - | > \"a\" \"hi\"";//"he\'\"\"\'e\' cho hi\'|hi >>there";
-// 	// char *s = " echo hi \"\" there\"     s\"\"\'$x\"";
-// 	// char *s = "ec\"\"\'\'ho";
-// 	printf("%s\n", s);
-// 	// printf("%d\n\n", get_num_tokens(s));
-// 	// t_token **r = tokenize(s);
-// 	t_token **r = parse(s);
-// 	int i = 0;
-// 	while (r && r[i])
-// 	{
-// 		printf("token: %s, type: %u\n", r[i]->token, r[i]->type);
-// 		free(r[i]->token);
-// 		free(r[i]);
-// 		i++;
-// 	}
-// 	printf("i: %d\n", i);
-// 	free(r);
-// }
