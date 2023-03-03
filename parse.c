08fe6d33db83adc4b610b19f1eabe37ca331a0e6
Youssef <youssefeltoukhi@hotmail.com>
2023-03-03 16:00:14 +0400

    parse.c checks for empty expansinos

--- parse.c
+++ parse.c
@@ -3,14 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: youssef <youssef@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yel-touk <yel-touk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 12:04:45 by yel-touk          #+#    #+#             */
-/*   Updated: 2023/03/03 14:52:40 by youssef          ###   ########.fr       */
+/*   Updated: 2023/03/03 15:56:47 by yel-touk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	check_empty_expansion(t_token **token)
+{
+	if ((*token)->type == empty_expansion && ft_strcmp((*token)->token, "\0"))
+		(*token)->type = cmd;
+}
+
+t_token	**parse(char *line, t_list *lst, t_ints *t_int)
+{
+	t_token	**tokens;
+	char	*temp;
+	int		i;
+
+	tokens = tokenize(line);
+	if (!tokens)
+		return (NULL);
+	if (!tokens[0])
+		return (tokens);
+	if (label_tokens(&tokens))
+	{
+		free_tokens(&tokens);
+		return (NULL);
+	}
+	i = 0;
+	while (tokens[i])
+	{
+		temp = tokens[i]->token;
+		tokens[i]->token = expand(&tokens[i], lst, t_int);
+		check_empty_expansion(&tokens[i]);
+		free(temp);
+		i++;
+	}
+	return (tokens);
+}
+
+	// i = 0;
+	// while (tokens && tokens[i])
+	// {
+	// 	printf("token: %s, type: %u\n", tokens[i]->token, tokens[i]->type);
+	// 	i++;
+	// }
 
 // t_token	**parse(char *line, t_list *lst, t_ints *t_int)
 // {
@@ -42,37 +83,3 @@
 // 	}
 // 	return (tokens);
 // }
-
-t_token	**parse(char *line, t_list *lst, t_ints *t_int)
-{
-	t_token	**tokens;
-	char*	temp;
-	int		i;
-
-	tokens = tokenize(line);
-	if (!tokens)
-		return (NULL);
-	if (!tokens[0])
-		return (tokens);
-	if (label_tokens(&tokens))
-	{
-		free_tokens(&tokens);
-		return (NULL);
-	}
-	i = 0;
-	while(tokens[i])
-	{
-		temp = tokens[i]->token;
-		tokens[i]->token = expand(&tokens[i], lst, t_int);
-		free(temp);
-		i++;
-	}
-	return (tokens);
-}
-
-	// int i = 0;
-	// while (tokens && tokens[i])
-	// {
-	// 	printf("token: %s, type: %u\n", tokens[i]->token, tokens[i]->type);
-	// 	i++;
-	// }
